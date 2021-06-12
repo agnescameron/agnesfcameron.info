26a50cce7e53b0f4ef9510e36431bec3e2bc4f79
Agnes Cameron <agnesfcameron@gmail.com>
2021-06-12 20:14:13 +0100

    added bell

--- assets/shrink_images.sh
+++ assets/shrink_images.sh
@@ -1,14 +1,14 @@
-find ./images-test -type f -name '*.jpg'  -size +1500k -exec bash -c 'convert -strip -quality 30% "$1" "$1" ' bash  {} \;
-find ./images-test -type f -name '*.jpg'  -size +1000k -exec bash -c 'convert -strip -quality 40% "$1" "$1" ' bash  {} \;
-find ./images-test -type f -name '*.jpg'  -size +500k -exec bash -c 'convert -strip -quality 50% "$1" "$1" ' bash  {} \;
-find ./images-test -type f -name '*.jpg'  -size +300k -exec bash -c 'convert -strip -quality 70% "$1" "$1" ' bash  {} \;
+find ./img -type f -name '*.jpg'  -size +1500k -exec bash -c 'convert -strip -quality 30% "$1" "$1" ' bash  {} \;
+find ./img -type f -name '*.jpg'  -size +1000k -exec bash -c 'convert -strip -quality 40% "$1" "$1" ' bash  {} \;
+find ./img -type f -name '*.jpg'  -size +600k -exec bash -c 'convert -strip -quality 60% "$1" "$1" ' bash  {} \;
+find ./img -type f -name '*.jpg'  -size +300k -exec bash -c 'convert -strip -quality 70% "$1" "$1" ' bash  {} \;
 
-find ./images-test -type f -name '*.png'  -size +1500k -exec bash -c 'convert -strip -quality 30% "$1" "$1" ' bash  {} \;
-find ./images-test -type f -name '*.png'  -size +1000k -exec bash -c 'convert -strip -quality 40% "$1" "$1" ' bash  {} \;
-find ./images-test -type f -name '*.png'  -size +500k -exec bash -c 'convert -strip -quality 50% "$1" "$1" ' bash  {} \;
-find ./images-test -type f -name '*.png'  -size +300k -exec bash -c 'convert -strip -quality 70% "$1" "$1" ' bash  {} \;
+find ./img -type f -name '*.png'  -size +1500k -exec bash -c 'convert -strip -quality 30% "$1" "$1" ' bash  {} \;
+find ./img -type f -name '*.png'  -size +1000k -exec bash -c 'convert -strip -quality 40% "$1" "$1" ' bash  {} \;
+find ./img -type f -name '*.png'  -size +600k -exec bash -c 'convert -strip -quality 60% "$1" "$1" ' bash  {} \;
+find ./img -type f -name '*.png'  -size +300k -exec bash -c 'convert -strip -quality 70% "$1" "$1" ' bash  {} \;
 
-find ./images-test -type f -name '*.JPG'  -size +1500k -exec bash -c 'convert -strip -quality 30% "$1" "$1" ' bash  {} \;
-find ./images-test -type f -name '*.JPG'  -size +1000k -exec bash -c 'convert -strip -quality 40% "$1" "$1" ' bash  {} \;
-find ./images-test -type f -name '*.JPG'  -size +500k -exec bash -c 'convert -strip -quality 50% "$1" "$1" ' bash  {} \;
-find ./images-test -type f -name '*.JPG'  -size +300k -exec bash -c 'convert -strip -quality 70% "$1" "$1" ' bash  {} \;
+find ./img -type f -name '*.JPG'  -size +1500k -exec bash -c 'convert -strip -quality 30% "$1" "$1" ' bash  {} \;
+find ./img -type f -name '*.JPG'  -size +1000k -exec bash -c 'convert -strip -quality 40% "$1" "$1" ' bash  {} \;
+find ./img -type f -name '*.JPG'  -size +600k -exec bash -c 'convert -strip -quality 60% "$1" "$1" ' bash  {} \;
+find ./img -type f -name '*.JPG'  -size +300k -exec bash -c 'convert -strip -quality 70% "$1" "$1" ' bash  {} \;
--- collections/_projects/bell.md
+++ collections/_projects/bell.md
@@ -3,6 +3,15 @@ layout: page
 title: An Introduction to the Bell System
 permalink: /bell/
 status: published
-image: 
+img: img/bell/carryingplan9books.jpg
+as: Foreign Objects
+date: 2021-03-17
 ---
 
+As Foreign Objects, + in collaboration with Eliza Collin (garment production) and Genie Kausto (styling and photography), I helped to research, design and fabricate a set of 5 embroidered jumpsuits based around the archives of the Computing Sciences Research Center at Bell Labs. This project was part of a program called [Further Experiments in Art and Technology](https://furtherexperiments.rhizome.org/), a collaboration between [Rhizome](https://rhizome.org/) and [Nokia Bell Labs](https://www.bell-labs.com/).
+
+I wrote in more detail about this project [here](https://soup.agnescameron.info/2021/03/21/bell-system.html), and the research for this project is located [here](https://www.are.na/foreign-objects/an-introduction-to-the-bell-system). I also 'designed' the [site](1127.foreignobjects.net/) for the project, which is just a folder full of images hosted over Apache.
+
+<img src="/assets/img/bell/carryingplan9books.jpg" width="50%" />
+<img src="/assets/img/bell/gremlin_desk.jpg" width="100%" />
+<img src="/assets/img/bell/suit_4_front.png" width="50%" />
